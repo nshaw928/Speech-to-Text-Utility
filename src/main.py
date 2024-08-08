ecd8ed4e0a72977388c59a7b132911182b428514
nshaw928 <nshaw928@gmail.com>
2024-08-07 22:13:56 -0400

    moved to src

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import customtkinter
+from baseFunctions import startTranscription
+
+
+# Create base GUI
+app = customtkinter.CTk()
+app.geometry("900x600")
+
+# Where to save the output text files
+
+
+# Upload audio file
+
+
+# Start
+button = customtkinter.CTkButton(app, text='Start Transciption', command='startTranscription')
+button.pack(padx=20, pady=20)
+
+# Status Bar
+
+
+# Start App
+app.mainloop()
